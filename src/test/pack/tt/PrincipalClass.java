9286659f9863dac3842878e01ebd5fabb8dc8319
Hmef <hmeftaah@gmail.com>
2022-02-08 04:01:38 +0100

    les classes restants

---
+++ src/test/pack/tt/PrincipalClass.java
@@ -0,0 +1,36 @@
+package test.pack.tt;
+
+
+import java.util.Map;
+
+public class PrincipalClass {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		String symptomfile = "symptoms.txt";
+		 
+		 SymptomList symptomlist = new SymptomList(symptomfile);
+		 
+		 AnalyticsCounter analyticscounter = new AnalyticsCounter();
+		 
+		 analyticscounter.calculNbrOccur(symptomlist.getSymptoms());
+		 
+		 Map<String, Integer> map = analyticscounter.calculNbrOccur(symptomlist.getSymptoms());
+		 
+		 Map<String, Integer> sortedmap = analyticscounter.mapSorting(map);
+		 
+		 analyticscounter.generateResultFile(sortedmap);
+		
+		 
+		 
+		 
+		 
+	}
+	
+	
+	
+	
+}
---
+++ src/test/pack/tt/SymptomListInterface.java
@@ -0,0 +1,17 @@
+package test.pack.tt;
+
+import java.util.List;
+
+public interface SymptomListInterface {
+
+
+	/**
+	 * getSymptoms() method 
+	 * 
+	 * @return return a list of symptoms from the file 
+	 * @exception catch the IOException generated and execute the code inside 
+	 * 
+	 */
+	
+	public List<String> getSymptoms();
+}
